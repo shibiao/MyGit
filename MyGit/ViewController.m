64b8a7ae4d904b5152e7e4f79ed250312881a81f
shibiao <956035125@qq.com>
2016-07-01 15:26:41 +0800

    2

--- MyGit/ViewController.m
+++ MyGit/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor=[UIColor redColor];
+    UIButton *btn=[UIButton buttonWithType:UIButtonTypeCustom];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
