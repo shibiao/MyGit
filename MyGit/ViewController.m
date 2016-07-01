593744cbe4975e39f7bf3d5eb7aca7a0a9dccc38
shibiao <956035125@qq.com>
2016-07-01 15:27:36 +0800

    3

--- MyGit/ViewController.m
+++ MyGit/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     self.view.backgroundColor=[UIColor redColor];
     UIButton *btn=[UIButton buttonWithType:UIButtonTypeCustom];
+    [self.view addSubview:btn];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
